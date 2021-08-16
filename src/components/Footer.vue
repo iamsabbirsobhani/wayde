79f762bdd13a71afbc597aacc19919b089857476
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-08-16 23:35:07 +0600

    Footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="footer">
+    <div class="left">
+      <img src="../assets/WayedNavIco.png" alt="" />
+      <p>
+        You don’t want to miss the news on Lossless. Subscribe - we’ll only send
+        updates on our launch.
+      </p>
+      <form @submit.prevent="">
+        <q-input class="inp" outlined label="Email your email" type="email" required />
+        <q-btn class="btn" type="submit" size="16px" no-wrap color="black" label="Get updates" />
+      </form>
+    </div>
+    <div class="right">
+      <div class="social">
+        <h3>Social</h3>
+        <p>Twitter</p>
+        <p>Medium</p>
+        <p>Telegram</p>
+        <p>GitHub</p>
+      </div>
+      <div class="lossless">
+        <h3>Lossless</h3>
+        <p>Technology</p>
+        <p>Whitepaper</p>
+      </div>
+      <div class="getin">
+        <h3>Get in Touch</h3>
+        <p>hello@wayde.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="foot-bottom">
+    <!-- <div class="foot-b-left"> -->
+    <p>Copyright © 2021 Wayde</p>
+    <p>Terms & Conditions</p>
+    <p>Privacy Policy</p>
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  margin-top: 150px;
+  display: flex;
+  background: white;
+  justify-content: space-between;
+  align-items: center;
+  border-top-right-radius: 50px;
+  padding: 80px;
+  width: 100%;
+}
+.right {
+  display: flex;
+  max-width: 500px;
+  h3 {
+      font-size: 20px;
+      font-weight: 700;
+  }
+  p {
+      cursor: pointer;
+      font-weight: 350;
+  }
+  .social {
+      margin-right: 20px;
+  }
+  .lossless {
+      margin-right: 20px;
+  }
+  .getin {
+      margin-right: 40px;
+  }
+}
+.left {
+  max-width: 500px;
+  img {
+    max-width: 180px;
+  }
+  form {
+      display: flex;
+  }
+  p {
+    font-size: 18px;
+    color: #394047;
+  }
+  .btn {
+      width: 150px;
+      margin-left: 20px;
+  }
+  .inp {
+      width: 250px;
+  }
+}
+.foot-bottom {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: white;
+  p {
+    text-align: center;
+    cursor: pointer;
+    font-size: 16px;
+    color: #394047;
+    margin: 5px;
+  }
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,9 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <Navbar/>
-    <FrontPage/>
-    <ExploreOffer/>
+    <Navbar />
+    <FrontPage />
+    <ExploreOffer />
+    <Footer/>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -10,22 +11,26 @@
 </template>
 
 <script>
-import Navbar from '../components/Navbar'
-import FrontPage from '../components/FrontPage'
-import ExploreOffer from '../components/ExploreOffer.vue'
+import Navbar from "../components/Navbar";
+import FrontPage from "../components/FrontPage";
+import ExploreOffer from "../components/ExploreOffer.vue";
+import Footer from "../components/Footer.vue";
 export default {
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
     Navbar,
     FrontPage,
-    ExploreOffer
+    ExploreOffer,
+    Footer
   },
 
-  setup () {
-
-    return {
-    }
-  }
-}
+  setup() {
+    return {};
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+
+</style>
