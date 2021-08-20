e1870eb7c1a10709315d89c6acff70de3beb80b3
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-08-20 20:12:31 +0600

    big fixed

--- src/components/TopContactNo.vue
+++ src/components/TopContactNo.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="topcard">
     <q-banner inline-actions rounded class="bg-amber-10 text-white">
+    <!-- <q-banner inline-actions rounded class="bg-indigo-3 text-white"> -->
       <!-- You have lost connection to the internet. This app is offline. -->
       <!-- <p>Contacts</p> -->
       <div class="topno">
-          <!-- <p>US</p> -->
-          <div  class="one">
-      <!-- <p class="ijk">(USA)</p> -->
-      <p href="tel:+91-0484-4148299" >
-        <i class="las la-phone-volume"></i> +17867233217
-      </p>
-      </div>
-      <!-- <p>IN</p> -->
+        <!-- <p>US</p> -->
+        <p>Contact Us:</p>
+        <div class="one">
+          <!-- <p class="ijk">(USA)</p> -->
+          <p href="tel:+91-0484-4148299">
+            <i class="las la-phone-volume"></i> +17867233217
+          </p>
+        </div>
+        <!-- <p>IN</p> -->
 
-      <div class="two">
-      <!-- <p class="ijk">(INDIA)</p> -->
-      <p href="tel:+91-0484-4148299">
-        <i class="las la-phone-volume"></i> +91 4844148299
-      </p>
-      </div>
+        <div class="two">
+          <!-- <p class="ijk">(INDIA)</p> -->
+          <p href="tel:+91-0484-4148299">
+            <i class="las la-phone-volume"></i> +91 4844148299
+          </p>
+        </div>
       </div>
       <!-- <template v-slot:action>
         <q-btn flat label="Turn ON Wifi" />
@@ -28,31 +30,30 @@
   </div>
 </template>
 
-
 <style lang="scss" scoped>
-.topno{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
+.topno {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
 }
-p{
-    margin-left: 10px;
-}
-
-.ijk{
-    p{
-        text-align: center;
-    }
+p {
+  margin-left: 10px;
 }
 
-.topcard{
-    max-width: 400px;
-    padding: 0;
-    transition: all 0.24s;
+.ijk {
+  p {
+    text-align: center;
+  }
 }
 
-.topcard:hover{
-    transform: scale(1.02);
+.topcard {
+  max-width: 400px;
+  padding: 0;
+  transition: all 0.24s;
+}
+
+.topcard:hover {
+  transform: scale(1.02);
 }
 </style>
